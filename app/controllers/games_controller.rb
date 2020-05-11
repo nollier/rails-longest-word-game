db9d3158b19ea3b38289b373d07d17e8454091ea
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-11 16:32:02 +0200

    game working but no score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     letter = ('A'..'Z').to_a
@@ -6,5 +9,32 @@ class GamesController < ApplicationController
       @letters << letter.sample
     end
     @letters
+  end
+
+  def score
+    @word = params[:word].downcase
+    @letters = params[:letters]
+
+    if valid_word?(@word, @letters) == true && word_exist?(@word) == true
+      @score = 'The word is valid according to the grid and is an English word'
+    elsif valid_word?(@word, @letters) == true
+      @score = 'The word is valid according to the grid, but is not a valid English word'
+    else
+      @score = 'The word can’t be built out of the original grid'
+    end
+  end
+
+  def word_exist?(word)
+    json = open("https://wagon-dictionary.herokuapp.com/#{word}").read
+    hash_word = JSON.parse(json)
+    if hash_word['found'] == true
+      return true
+    else
+      return false
+    end
+  end
+
+  def valid_word?(word, letters)
+    word.upcase.chars.all? { |letter| word.upcase.count(letter) <= letters.count(letter) }
   end
 end
